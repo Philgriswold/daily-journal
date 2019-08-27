5aa0dc6a02f399347db7a310b9fb04a73cf6dffc
Phil <Griswold@Phils-MacBook-Pro.local>
2019-08-27 15:57:22 -0500

    udated my daily-journal

---
+++ src/scripts/journal.js
@@ -0,0 +1,30 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+
+console.log("yo javascript whatup");
+const journalEntry = []
+
+journalEntry.push({
+    dateofEntry: "August, 26 2019",
+    conceptsCovered: "functions, objects",
+    entry: "we learned about objects, dot notation, and other Javascript functions.",
+    mood: "ok",
+})
+
+journalEntry.push({
+    dateofEntry: "August, 23 2019",
+    conceptsCovered: "Celebrity project presentation",
+    entry: "we presented our group projects and other Javascript functions",
+    mood: "ready for the weekend",
+})
+
+journalEntry.push({
+    dateofEntry: "August, 22 2019",
+    conceptsCovered: "We worked on our celebrity page",
+    entry: "we spent all day in labs for celebrity project",
+    mood: "ready for the weekend",
+})
+
+console.log("journalEntry", journalEntry);
